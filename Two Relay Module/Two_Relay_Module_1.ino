53127af3a05c1b9f778cc98f17f39a0ba27e1ef4
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-04-12 12:26:22 +0300

    Add files via upload

---
+++ Two Relay Module/Two_Relay_Module_1.ino
@@ -0,0 +1,22 @@
+int RelayPinOne = 12;
+int RelayPinTwo = 11;
+
+void setup() {
+	// Set RelayPin as an output pin
+	pinMode(RelayPinOne, OUTPUT);
+	pinMode(RelayPinTwo, OUTPUT);
+}
+
+void loop() {
+	// Let's turn on the relay...
+	digitalWrite(RelayPinOne, LOW);
+	delay(3000);
+	digitalWrite(RelayPinTwo, LOW);
+	delay(3000);
+	
+	// Let's turn off the relay...
+	digitalWrite(RelayPinOne, HIGH);
+	delay(3000);
+	digitalWrite(RelayPinTwo, HIGH);
+	delay(3000);
+}
