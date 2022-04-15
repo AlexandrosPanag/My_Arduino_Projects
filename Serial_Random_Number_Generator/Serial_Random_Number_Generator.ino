0927546e927296e3ad3c6ad8c93bbbc556ba0a84
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-04-15 15:57:55 +0300

    Add files via upload

---
+++ Serial_Random_Number_Generator/Serial_Random_Number_Generator.ino
@@ -0,0 +1,12 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  for(int i=1; i <=9; i++){
+    int r=random(0,100);
+    Serial.print(r);
+    Serial.print(" ");
+    }
+    Serial.println();
+}
