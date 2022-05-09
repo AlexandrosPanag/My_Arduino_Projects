d895156e4da8436af5be3a3586d926623c3645fb
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-05-09 11:14:57 +0300

    Add files via upload

---
+++ MQ_135_LCD/MQ_135_LCD.ino
@@ -0,0 +1,42 @@
+/*********
+ By Alexandros Panagiotakopoulos
+*********/
+
+#include <LiquidCrystal_I2C.h>
+
+int sensorValue;
+
+// set the LCD number of columns and rows
+int time_count_second=0; //Declaring a time counter for each second
+int lcdColumns = 16;
+int lcdRows = 2;
+
+// set LCD address, number of columns and rows
+// if you don't know your display address, run an I2C scanner sketch
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
+
+void setup(){
+  
+  // initialize LCD
+  lcd.init();
+  // turn on LCD backlight                      
+  lcd.backlight();
+  Serial.begin(9600); 
+}
+
+void loop(){
+  sensorValue = analogRead(0);       // read analog input pin 0
+  Serial.print("AirQua=");
+  Serial.print(sensorValue, DEC);               // prints the value read
+  Serial.println(" PPM");
+  //random number generator
+   time_count_second++; //adding each second on the time counter
+    
+  // set cursor to first column, first row
+  lcd.setCursor(0, 0);
+  lcd.print("ArQ=");
+  lcd.print(sensorValue,DEC);   // print message
+  lcd.print(" PPM");
+  delay(5000);    // Wait one second
+  lcd.clear();   // clears the display to print new message
+}
