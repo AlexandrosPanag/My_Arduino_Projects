767ccd4d8ee84907df00084914f684bf7991cdd4
Αλέξανδρος Παναγιωτακόπουλος <69578661+AlexandrosPanag@users.noreply.github.com>
2022-05-17 14:40:13 +0300

    Add files via upload

---
+++ PCT2075/PCT2075.ino
@@ -0,0 +1,30 @@
+/*
+ * Created by Alexandros Panagiotakopoulos
+*/
+
+#include <Adafruit_PCT2075.h> //Adafruit PCT2075 by Adafruit
+
+Adafruit_PCT2075 PCT2075; //declare the PCT2075 Object - variable
+
+void setup() {
+  PCT2075 = Adafruit_PCT2075(); //initialize the object - variable
+  Serial.begin(115200); //begin the serial baud rate to 115200 baud
+  // Wait until serial port is opened
+  while (!Serial) { delay(1); }
+  Serial.println("Adafruit PCT2075 Test");
+
+  if (!PCT2075.begin()) { //check if the PCT2075 is connected properly or was found
+    Serial.println("Couldn't find PCT2075 sensor"); //if it's not connected properly or wasn't found
+    while (1); //stay on the loop
+  }  
+  Serial.println("Found PCT2075 sensor"); //otherwise, print that the sensor was found
+}
+
+void loop() { //print the serial temperature
+  Serial.println("***************************************************");
+  Serial.print("Temperature: "); 
+  Serial.print(PCT2075.getTemperature());
+  Serial.println(" C");
+  Serial.println();
+  delay(10000);
+}
