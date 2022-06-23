f7da49c6cf17df3d3066d2e98ba2b8135b2a4762
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2022-06-23 14:05:46 +0300

    Update Air_Quality_Project.ino

--- Air_Quality_Project/Air_Quality_Project.ino
+++ Air_Quality_Project/Air_Quality_Project.ino
@@ -3,7 +3,34 @@
 *********/
 
 #include <LiquidCrystal_I2C.h>
+/*
+Author
+Frank de Brabander
+Maintainer
+Marco Schwartz
+Website
+https://github.com/marcoschwartz/LiquidCrystal_I2C
+avr
+*/
+
 #include "dht.h" 
+/*!
+ *  @file DHT.h
+ *
+ *  This is a library for DHT series of low cost temperature/humidity sensors.
+ *
+ *  You must have Adafruit Unified Sensor Library library installed to use this
+ * class.
+ *
+ *  Adafruit invests time and resources providing this open source code,
+ *  please support Adafruit andopen-source hardware by purchasing products
+ *  from Adafruit!
+ *
+ *  Written by Adafruit Industries.
+ *
+ *  MIT license, all text above must be included in any redistribution
+ */
+
 dht DHT;
 
 int sensorValue;
