5b74df542f394e4107ae40de02f4c56eebb567aa
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2024-07-29 12:41:34 +0300

    Update Potentiometer_LED.ino

--- B10K Potentiometer/Potentiometer_LED.ino
+++ B10K Potentiometer/Potentiometer_LED.ino
@@ -1,16 +1,11 @@
 /*  Created By Alexandros Panagiotakopoulos - alexandrospanag.github.io                                                  */
    
-
-//Variables:
 int value; //save analog value
 
 
 void setup(){
-  //Input or output?
-  pinMode(9, OUTPUT); //Potentiometer PIN
   pinMode(A2, INPUT); //LED Pin 
   Serial.begin(9600); //Initialize Serial Monitior (OPTIONAL) to check the values.
-
 }
 
 void loop(){
