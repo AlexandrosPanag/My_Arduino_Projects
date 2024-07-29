f8a3eedd1c3dc7d6783a75b0e7f36276754d1f01
Alexandros Panagiotakopoulos <69578661+AlexandrosPanag@users.noreply.github.com>
2024-07-29 12:38:56 +0300

    Update Potentiometer_LED.ino

--- B10K Potentiometer/Potentiometer_LED.ino
+++ B10K Potentiometer/Potentiometer_LED.ino
@@ -14,9 +14,9 @@ void setup(){
 }
 
 void loop(){
-  value = analogRead(9);          //Read and save analog value from potentiometer
+  value = analogRead(A2);          //Read and save analog value from potentiometer
   value = map(value, 0, 1023, 0, 255); //Map value 0-1023 to 0-255 (PWM)
   analogWrite(9, value);          //Send PWM value to led
-  delay(1000);                          //Small delay
+  delay(100);                          //Small delay
   Serial.println(value); //Check the volume 0 through the Serial 0 for OFF (minimum lighting volume), 255 for ON (maximum lighting volume) 
 }
